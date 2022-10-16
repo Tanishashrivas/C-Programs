32662a064b6d87922438ffeb526cfc0376f24149
Tanisha <114223066+Tanisha-S02@users.noreply.github.com>
2022-10-16 23:45:21 +0530

    Initial commit

---
+++ Project/Guess the number game.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main(){
+	int number, guess, guesses=1;
+	number = rand()%100 + 1;
+	srand(time(0));
+	
+	printf("guess the number between 1 to 100\n");
+	do{
+	scanf("%d", &guess);
+	
+		if(guess>number){
+			printf("lower number please!\n");
+		}
+		else if(guess<number){
+			printf("higher number please!\n");
+		}
+		else{
+			printf("you guessed it in %d attempts\n", guesses);
+	}
+	guesses++;
+	}while(guess!=number);
+	return 0;
+}
